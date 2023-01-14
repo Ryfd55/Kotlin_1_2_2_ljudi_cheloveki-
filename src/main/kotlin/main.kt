7a63ead8767046dc90330b3bbca44444506d2624
Denis Gubardin <gubardin1984@mail.ru>
2023-01-14 13:38:19 +0400

    fixed an exception "11"

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,10 +2,26 @@ fun main() {
     println("Введите количество лайков:")
     val likes: Int =
         readLine()!!.toInt()
-val res = getCheloveki(likes)
-        print("Понравилось $likes $res")
+    val res = getCheloveki(likes)
+    print("Понравилось $likes $res")
 }
 
-fun getCheloveki (amount: Int): String= if (amount%10 == 1)
-    "человеку"
-    else  "человекам"
+fun getCheloveki(amount: Int): String {
+    var am1 = amount
+    var count = 0
+    for (i in 1..2) {
+        am1 %= 10
+        if (am1 != 1)
+            break else
+            count++
+        am1 = amount / 10
+    }
+    return if (count == 2) {
+        "человекам"
+    } else {
+        if (amount % 10 == 1)
+            "человеку"
+        else "человекам"
+    }
+}
+
